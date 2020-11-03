8c54adbec5d181d30b2264b4e9d614f2f4321ebd
DinaraS <dinarkaforsik@mail.ru>
2020-11-03 23:32:00 +0300

    hws

---
+++ hw1_5.py
@@ -0,0 +1,21 @@
+rev = 0
+exp = 0
+rent_prof = 0
+prof = 0
+staff_prof = 0
+staff = 0
+rev = int(input("Please insert revenue: "))
+exp = int(input("Please insert expenses: "))
+if rev > exp:
+    print("Good job! We have a profit this year!")
+    prof = rev - exp
+    rent_prof = (prof / rev) * 100
+    staff = int(input("Please insert number of staff: "))
+    staff_prof = prof // staff
+    print("Profit of this year is", prof)
+    print(f"Profitability is {rent_prof}%")
+    print("Profit on staff is", staff_prof)
+elif rev == exp:
+    print("We have a zero")
+else:
+    print("Bad news, we have a loss")
---
+++ hw1_6.py
@@ -0,0 +1,12 @@
+days = 1
+km = 0
+req_km = 0
+km = int(input("Please insert count of kms in first day: "))
+req_km = int(input("Please insert count of required kms: "))
+while km < req_km:
+    a1 = km + ((km * 10) / 100)
+    km = a1
+    days = days + 1
+    continue
+if km >= req_km:
+    print(f"Requested kms will be reached on day {days}")
